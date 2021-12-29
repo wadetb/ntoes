c8bb49fa888795413429e83065973cd6309f5b2b
Wade Brainerd <wadetb@gmail.com>
2021-12-28 23:10:34 -0500

    refactoring and improvements

--- ntoes.py
+++ ntoes.py
@@ -17,6 +17,111 @@ MARKDOWN_SYNTAX = 'Packages/Markdown/Markdown.sublime-syntax'
 SCAN_PERIOD = 10
 
 
+class TodoList:
+	def __init__(self):
+		self.note_files = {}
+		self.cancel_scanning = False
+		self.wakeup_event = threading.Event()
+		threading.Thread(target=self.scan_forever, daemon=True).start()
+
+	def get_base_dir(self):
+		s = sublime.load_settings("Ntoes.sublime-settings")
+		base_dir = s.get("base_dir", "~/ntoes/")
+		base_dir = os.path.expanduser(base_dir)
+		return base_dir
+
+	def get_note_dir_from_title(self, title):
+		note_date = datetime.datetime.strptime(title[:7], '%Y-%m')
+		note_dir = self.get_base_dir()
+		note_dir = os.path.join(note_dir, str(note_date.year))
+		note_dir = os.path.join(note_dir, str(note_date.strftime('%m - %B')))
+		return note_dir
+
+	def get_todo_view(self):
+		for window in sublime.windows():
+			for view in window.views():
+				if view.settings().get('is_todo') is not None:
+					return view
+		return None
+
+	def update_todo_view(self):
+		todo_view = self.get_todo_view()
+
+		text = ''
+		for file_path in sorted(self.note_files.keys(), reverse=True):
+			fields = self.note_files[file_path]
+			if len(fields['todos']):
+				text += '# {}\n\n'.format(os.path.basename(file_path))
+				for todo in fields['todos']:
+					text += todo['text']
+				text += '\n'
+
+		todo_view.run_command('update_todo_view', {'text': text})
+		# todo_view.run_command('move_to', {"extend": False, "to": "bol"})
+		# todo_view.run_command('select_all')
+		# todo_view.run_command('overwrite', {'characters': text})
+
+	def stop_scanning(self):
+		self.cancel_scanning = True
+
+	def scan_now(self):
+		self.wakeup_event.set()
+
+	def add_new_note_file(self, file_path):
+		st = os.stat(file_path)
+		self.note_files[file_path] = {
+			"mtime": st.st_mtime,
+			"todos": []
+		}
+
+	def scan_file(self, file_path):
+		print('SCAN', file_path)
+		
+		todos = []
+		with open(file_path, encoding='utf-8', errors='ignore') as file:
+			for line_index, line in enumerate(file.readlines()):
+				if '[ ]' in line:
+					todos.append({'line': line_index, 'text': line})
+
+		self.note_files[file_path]['todos'] = todos
+
+	def scan_dir(self):
+		base_dir = self.get_base_dir()
+		note_paths = []
+
+		for dirpath, _, filenames in os.walk(base_dir):
+			for file_name in filenames:
+				if file_name.endswith('.md'):
+					file_path = os.path.join(dirpath, file_name)
+					note_paths.append(file_path)
+
+		for file_path in sorted(note_paths, reverse=True):
+			st = os.stat(file_path)
+
+			if file_path in self.note_files:
+				fields = self.note_files[file_path]
+				if st.st_mtime <= fields["mtime"]:
+					continue
+				fields["mtime"] = st.st_mtime
+
+			else:
+				self.add_new_note_file(file_path)
+
+			self.scan_file(file_path)
+		
+		self.update_todo_view()
+
+	def scan_forever(self):
+		while not self.cancel_scanning:
+			if self.get_todo_view(): # Only scan if todo list is visible
+				self.scan_dir()
+			self.wakeup_event.wait(SCAN_PERIOD)
+			self.wakeup_event.clear()
+
+
+todo_list = TodoList()
+
+
 class NewNoteCommand(sublime_plugin.WindowCommand):
 	def run(self):
 		self.window.show_input_panel("New Note", datetime.date.today().isoformat(), self.on_new_note, None, None)
@@ -28,71 +133,76 @@ class NewNoteCommand(sublime_plugin.WindowCommand):
 		note_view.run_command('move_to', {'to': 'eof', 'extend': False})
 		note_view.assign_syntax(MARKDOWN_SYNTAX)
 
-		s = sublime.load_settings("Ntoes.sublime-settings")
+		note_dir = todo_list.get_note_dir_from_title(title)
+		os.makedirs(note_dir, 0o777, True)
+		note_view.settings().set('default_dir', note_dir)
+		note_view.settings().set('base_dir', todo_list.get_base_dir())
 
-		note_date = datetime.datetime.strptime(title[:7], '%Y-%m')
-
-		base_dir = s.get("base_dir", "~/ntoes/")
-		base_dir = os.path.expanduser(base_dir)
-		base_dir = os.path.join(base_dir, str(note_date.year))
-		base_dir = os.path.join(base_dir, str(note_date.strftime('%m - %B')))
-
-		os.makedirs(base_dir, 0o777, True)
-
-		note_view.settings().set('default_dir', base_dir)
-		note_view.settings().set('base_dir', s.get("base_dir", "~/ntoes/"))
+	def description(self):
+		return "Creates a new note using the current date as a starting point."
 
 
 class ShowNoteCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
-		s = self.view.settings()
+		todo_view = todo_list.get_todo_view()
 
-		for region in self.view.sel():
-			todo_line = self.view.line(region.a)
-			todo_text = self.view.substr(todo_line)
-			print(todo_line, todo_text)
+		# Start with the current line.
+		region = todo_view.sel()[0]
+		todo_line = todo_view.line(region.a)
+		todo_text = todo_view.substr(todo_line)
+		# print(todo_line, todo_text)
 
-			line_re = re.compile(r'\[.\] (.*)')
-			todo_m = line_re.match(todo_text)
-			if not todo_m:
-				print('Not a TODO line, no sync.')
-				return
-			todo_desc = todo_m.group(1)
+		# Split the line into "[?] Blah blah..."
+		line_re = re.compile(r'\[.\] (.*)')
+		todo_m = line_re.match(todo_text)
+		if not todo_m:
+			print('Not a TODO line, no sync.')
+			return
+		todo_desc = todo_m.group(1)
 
-			file_heading_line, file_heading_text = todo_line, None
-			while file_heading_line.a > 0:
-				file_heading_line = self.view.line(sublime.Region(file_heading_line.a - 1))
-				file_heading_text = self.view.substr(file_heading_line)
-				if file_heading_text.startswith('# '):
-					break
-			if file_heading_text is None:
-				print('No file header for this TODO.')
-				return
-			file_name = file_heading_text[2:]
+		# Scan upwards for a heading like "# YYYY-MM-DD Title.md"
+		file_heading_line, file_heading_text = todo_line, None
+		while file_heading_line.a > 0:
+			file_heading_line = todo_view.line(sublime.Region(file_heading_line.a - 1))
+			file_heading_text = todo_view.substr(file_heading_line)
+			if file_heading_text.startswith('# '):
+				break
+		if file_heading_text is None:
+			print('No file header for this TODO.')
+			return
+		file_name = file_heading_text[2:]
 
-			note_date = datetime.datetime.strptime(file_name[:7], '%Y-%m')
-			base_dir = s.get("todo_base_dir")
-			base_dir = os.path.expanduser(base_dir)
-			base_dir = os.path.join(base_dir, str(note_date.year))
-			base_dir = os.path.join(base_dir, str(note_date.strftime('%m - %B')))
+		# Read the file, finding and updating the line in question.			
+		note_dir = todo_list.get_note_dir_from_title(file_name)
+		file_path = os.path.join(note_dir, file_name)
+		# any_changes = False
 
-			file_path = os.path.join(base_dir, file_name)
-			# print(file_heading_line, file_heading_text, file_name, file_path)
+		with open(file_path, encoding='utf-8', errors='ignore') as file:
+			all_lines = file.readlines()
 			
-			with open(file_path) as file:
-				all_lines = file.readlines()
-				
-				for line_index, line in enumerate(all_lines):
-					line_m = line_re.match(line)
-					if line_m:
-						line_desc = line_m.group(1)
-						if line_desc == todo_desc:
-							print('found at', line_index, line_desc)
-							all_lines[line_index] = todo_text + '\n'
-							break
+			for line_index, line in enumerate(all_lines):
+				line_m = line_re.match(line)
+				if line_m:
+					line_desc = line_m.group(1)
+					if line_desc == todo_desc:
+						# print('found at', line_index, line_desc)
+						new_line = todo_text + '\n'
+						if all_lines[line_index] != new_line:
+							all_lines[line_index] = new_line
+							# any_changes = True
+						break
 
-			self.view.window().open_file("{}:{}:{}".format(file_path, line_index + 1, 0), sublime.ENCODED_POSITION, 0)
-			# sublime.run_command('move_to_group', {'group': 0})
+		# # Write the file back with the modified line
+		# if any_changes:
+		# 	with open(file_path, 'w', encoding='utf-8') as file:
+		# 		file.writelines(all_lines)
+
+		# Navigate the window to the corresponding note.
+		todo_view.window().open_file("{}:{}:{}".format(file_path, line_index + 1, 0), sublime.ENCODED_POSITION, 0)
+		# sublime.run_command('move_to_group', {'group': 0})
+
+	def description(self):
+		return "In the TODO view, navigates to the corresponding note."
 
 
 class MakeTodoCommand(sublime_plugin.TextCommand):
@@ -117,7 +227,7 @@ class MakeTodoCommand(sublime_plugin.TextCommand):
 				self.view.insert(edit, region.begin(), '[ ] ')
 
 	def description(self):
-		return "Toggles the current line's TODO item status."
+		return "Toggles the selected lines todo state."
 
 
 class UpdateTodoViewCommand(sublime_plugin.TextCommand):
@@ -126,180 +236,40 @@ class UpdateTodoViewCommand(sublime_plugin.TextCommand):
 		self.view.run_command('move_to', {"extend": False, "to": "bol"})
 
 
-class TodoList:
-	def __init__(self):
-		self.note_files = {}
-		self.todo_view = None
-		self.scan_thread = None
-		self.cancel_scanning = False
-		self.wakeup_event = threading.Event()
+# Experiment with making changes in the todo view transfer to the file
+# being edited: too noisy.
 
-	def start_scanning(self):
-		if self.scan_thread is None:
-			self.scan_thread = threading.Thread(target=self.scan_forever, daemon=True)
-			self.scan_thread.start()
+# class TodoViewEventListener(sublime_plugin.ViewEventListener):
+# 	@classmethod
+# 	def is_applicable(cls, settings):
+# 		return settings.get('is_todo') is not None
 
-	def stop_scanning(self):
-		self.cancel_scanning = True
-
-	def is_scanning(self):
-		return self.scan_thread is not None
-
-	def add_note_file(self, file_path):
-		st = os.stat(file_path)
-		self.note_files[file_path] = {
-			"mtime": st.st_mtime,
-			"todos": []
-		}
-
-	def scan_file(self, file_path):
-		print('SCAN', file_path)
-		todos = []
-		with open(file_path, encoding='utf-8', errors='ignore') as file:
-			for line_index, line in enumerate(file.readlines()):
-				if '[ ]' in line:
-					todos.append({'line': line_index, 'text': line})
-
-		self.note_files[file_path]['todos'] = todos
-
-	def update_view(self):
-		if self.todo_view is None:
-			return
-
-		text = ''
-		for file_path in sorted(self.note_files.keys(), reverse=True):
-			fields = self.note_files[file_path]
-			if len(fields['todos']):
-				text += '# {}\n\n'.format(os.path.basename(file_path))
-				for todo in fields['todos']:
-					text += todo['text']
-				text += '\n'
-
-		self.todo_view.run_command('update_todo_view', {'text': text})
-		# self.todo_view.run_command('move_to', {"extend": False, "to": "bol"})
-		# self.todo_view.run_command('select_all')
-		# self.todo_view.run_command('overwrite', {'characters': text})
-
-	def scan_dir(self):
-		print('SCAN_DIR')
-		s = sublime.load_settings("Ntoes.sublime-settings")
-		base_dir = s.get("base_dir", "~/ntoes/")
-		base_dir = os.path.expanduser(base_dir)
-
-		note_paths = []
-
-		for dirpath, _, filenames in os.walk(base_dir):
-			for file_name in filenames:
-				if file_name.endswith('.md'):
-					file_path = os.path.join(dirpath, file_name)
-					note_paths.append(file_path)
-
-		for file_path in sorted(note_paths, reverse=True):
-			st = os.stat(file_path)
-
-			if file_path in self.note_files:
-				fields = self.note_files[file_path]
-				if st.st_mtime <= fields["mtime"]:
-					continue
-				fields["mtime"] = st.st_mtime
-
-			else:
-				self.add_note_file(file_path)
-
-			self.scan_file(file_path)
-			self.update_view()
-
-	def scan_forever(self):
-		while not self.cancel_scanning:
-			self.scan_dir()
-			self.wakeup_event.wait(SCAN_PERIOD)
-			self.wakeup_event.clear()
+# 	def on_modified(self):
+# 		todo_list.sync_note_with_todo()
 
 
-todo_list = TodoList()
-
-
-class TodoListener(sublime_plugin.ViewEventListener):
-	@classmethod
-	def is_applicable(cls, settings):
-		# print('is_applicable', settings, settings.get('is_todo'))
-		return settings.get('todo_base_dir') is not None
-
-	def on_modified(self):
-		# print('on_modified', self)
-		# print(self.view.sel())
-
-		s = self.view.settings()
-
-		for region in self.view.sel():
-			todo_line = self.view.line(region.a)
-			todo_text = self.view.substr(todo_line)
-			# print(todo_line, todo_text)
-
-			line_re = re.compile(r'\[.\] (.*)')
-			todo_m = line_re.match(todo_text)
-			if not todo_m:
-				print('Not a TODO line, no sync.')
-				return
-			todo_desc = todo_m.group(1)
-
-			file_heading_line, file_heading_text = todo_line, None
-			while file_heading_line.a > 0:
-				file_heading_line = self.view.line(sublime.Region(file_heading_line.a - 1))
-				file_heading_text = self.view.substr(file_heading_line)
-				if file_heading_text.startswith('# '):
-					break
-			if file_heading_text is None:
-				print('No file header for this TODO.')
-				return
-			file_name = file_heading_text[2:]
-
-			note_date = datetime.datetime.strptime(file_name[:7], '%Y-%m')
-			base_dir = s.get("todo_base_dir")
-			base_dir = os.path.expanduser(base_dir)
-			base_dir = os.path.join(base_dir, str(note_date.year))
-			base_dir = os.path.join(base_dir, str(note_date.strftime('%m - %B')))
-
-			file_path = os.path.join(base_dir, file_name)
-			# print(file_heading_line, file_heading_text, file_name, file_path)
-			
-			with open(file_path) as file:
-				all_lines = file.readlines()
-				
-				for line_index, line in enumerate(all_lines):
-					line_m = line_re.match(line)
-					if line_m:
-						line_desc = line_m.group(1)
-						if line_desc == todo_desc:
-							# print('found at', line_index, line_desc)
-							all_lines[line_index] = todo_text + '\n'
-							break
-			# print(all_lines)
-
-			with open(file_path, 'w') as file:
-				file.writelines(all_lines)
+class NoteViewEventListener(sublime_plugin.ViewEventListener):
+	def on_post_save(self):
+		base_dir = os.path.normpath(todo_list.get_base_dir())
+		file_name = os.path.normpath(self.view.file_name())
+		if file_name.startswith(base_dir):
+			todo_list.scan_now()
 
 
 class ShowTodoCommand(sublime_plugin.WindowCommand):
 	def run(self):
-		todo_view = None
-		for view in self.window.views():
-			if view.name() == 'TODO':
-				todo_view = view
+		todo_view = todo_list.get_todo_view()
 
 		if todo_view is None:
 			todo_view = self.window.new_file()
 			todo_view.set_name('TODO')
-
+			todo_view.set_scratch(True)
 			todo_view.settings().set('gutter', False)
 			todo_view.settings().set('is_todo', True)
-
-			s = sublime.load_settings("Ntoes.sublime-settings")
-			todo_view.settings().set('todo_base_dir', s.get("base_dir", "~/ntoes/"))
-
 			todo_view.assign_syntax(MARKDOWN_SYNTAX)
 			todo_view.run_command('overwrite', {'characters': '# TODO\n\n'})
 			todo_view.run_command('move_to', {"extend": False, "to": "bof"})
+
 			self.window.run_command('new_pane')
 
 		self.window.run_command('set_layout', {
@@ -307,11 +277,10 @@ class ShowTodoCommand(sublime_plugin.WindowCommand):
 			"cols": [0.0, 0.7, 1.0], "rows": [0.0, 1.0]
 			})
 
-		if not todo_list.is_scanning():
-			todo_list.todo_view = todo_view
-			todo_list.start_scanning()
-		else:
-			todo_list.wakeup_event.set()
+		todo_list.scan_now()
+
+	def description(self):
+		return "Show todo items from all notes in a new view."
 
 
 class SetNotesDirCommand(sublime_plugin.WindowCommand):
@@ -319,19 +288,20 @@ class SetNotesDirCommand(sublime_plugin.WindowCommand):
 		self.window.show_input_panel('Notes Directory', "~/ntoes", self.on_set_notes_dir, None, None)
 
 	def on_set_notes_dir(self, base_dir):
-		s = sublime.load_settings("Ntoes.sublime-settings")
-
 		base_dir = os.path.expanduser(base_dir)
-
 		os.makedirs(base_dir, 0o777, True)
 
+		s = sublime.load_settings("Ntoes.sublime-settings")
 		s.set("base_dir", base_dir)
 
 		sublime.save_settings("Ntoes.sublime-settings")
 
+	def description(self):
+		return "Selects the base directory for note files."
+
 
 class SyncNotesCommand(sublime_plugin.WindowCommand):
-	def exec(self, cmd):
+	def exec_git(self, cmd):
 		print(f'$ {cmd}')
 		try:
 			p = subprocess.run(cmd, check=True, text=True, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
@@ -346,14 +316,17 @@ class SyncNotesCommand(sublime_plugin.WindowCommand):
 		base_dir = os.path.expanduser(base_dir)
 
 		os.chdir(base_dir)
-		self.exec(['git', 'fetch'])
-		self.exec(['git', 'merge'])
-		status = self.exec(['git', 'status', '--porcelain'])
+		self.exec_git(['git', 'fetch'])
+		self.exec_git(['git', 'merge'])
+		status = self.exec_git(['git', 'status', '--porcelain'])
 		if len(status) == 0:
 			return
-		self.exec(['git', 'add', '.'])
-		self.exec(['git', 'commit', '-m', status])
-		self.exec(['git', 'push'])
+		self.exec_git(['git', 'add', '.'])
+		self.exec_git(['git', 'commit', '-m', status])
+		self.exec_git(['git', 'push'])
+
+	def description(self):
+		return "Synchronize notes with the remote server."
 
 
 def plugin_unloaded():
